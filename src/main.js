53d3a436f94cd5f8ccef102ed318d7eb6cca5942
Sushao <suxiaoshao@gmail.com>
2020-04-03 17:35:32 +0800

    start

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+import axios from "axios"
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.prototype.axios = axios;
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    render: h => h(App)
+}).$mount('#app');
